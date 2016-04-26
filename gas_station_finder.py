e02cefcc0ac838b2fba0cf6e461537541c101a64
Lluc <lluc.pares@e-campus.uab.cat>
2016-04-26 20:10:37 +0200

    Added main function and messages changed

--- gas_station_finder.py
+++ gas_station_finder.py
@@ -8,14 +8,21 @@ import telebot
 import urllib2, urllib
 import json
 import ast
+import time
+
+# To avoid warnings in Python < 2.7.9. Not important!
+import requests
+requests.packages.urllib3.disable_warnings()
 
 # Token of the telegram Bot
 TOKEN_TELEGRAM = '130498184:AAFFJLIMxFV-UR8tlKbZeQi2wzU9Dckkr0w'
 # Linking the Bot to this python script
 bot = telebot.TeleBot(TOKEN_TELEGRAM)
 
+# API Key of Google Maps API
 KEY_PLACES = 'AIzaSyAgFyIkMlchQEzPyV3y6NR50MtZdfDvjic'
 
+# Gas Station class to save all its info
 class GasStation:
 	def __init__(self, name, address, price, type, city):
 		self.name = name
@@ -35,7 +42,8 @@ class GasStation:
 		return self.type
 	def __str__(self):
 		return str(self.name) + ':' + str(self.city) + ':' + str(self.price)
-	
+
+# Returns a set of coords (lat,lng) in Google Maps given an address name and a city
 def getMapLocation(address, city):
 	url = 'https://maps.googleapis.com/maps/api/geocode/json?'
 	params = { 'key' : KEY_PLACES, 'address' : address+' '+city }
@@ -47,16 +55,17 @@ def getMapLocation(address, city):
 	long = object_json['results'][0]['geometry']['location']['lng']
 	coords = [lat,long]
 	return coords
-	
 
-def getCities(input):
+# Returns a list with the cities that could match given an string of characters
+def getCities(input_string):
 	url = 'http://www.gasbuddy.com/Home/Autocomplete?'
-	params = { 'query' : input }
+	params = { 'query' : input_string }
 	url += urllib.urlencode(params)
 	content = urllib2.urlopen(url).read()
-	list = ast.literal_eval(content)
-	return list
+	list_cities = ast.literal_eval(content)
+	return list_cities
 
+# Gets info about a gas station given the results of the search
 def getStation(object_json):
 	cheapest_station = object_json['stations'][0]
 	
@@ -121,33 +130,38 @@ def query_with_text(inline_query):
 @bot.message_handler(commands=['help'])
 def helpCommand(message):
 	try:
-		bot.reply_to(message, "Send a location (USA or Canada) and you are done. Or type a name of a city (USA or Canada) and a menu will be displayed, select the city and Gas or Diesel. Alternatively you can enter the commands /gas and /diesel followed by the city name. Using the commands only the most popular city with that name will be displayed.")
+		bot.send_message(message.chat.id, "Send a location (USA or Canada) and you are done :D You can also use inline mode! Alternatively send a name of a city and a menu will be displayed to help you choose. If you want you can use the commands /gas and /diesel directly followed by a city name. Using the commands only the most popular city with that name will be displayed.")
 	except:
-		print "Error"
+		print "Error in help"
 		return 1
 
+# Function that gets called when the commands gas or diesel ar entered
 @bot.message_handler(commands=['gas', 'diesel'])
 def gasCommand(message):
 	command = message.text.split(' ', 1)[0]
 	try:
-		input = message.text.split(' ', 1)[1]
+		input_city = message.text.split(' ', 1)[1]
 	except:
-		bot.reply_to(message, "Add a city, ex: /gas New York")
+		bot.send_message(message.chat.id, "You should add a city, ex: /gas New York")
 		return 4
-	cityList = getCities(input)
+	try:
+		cityList = getCities(input_city)
+	except:
+		bot.send_message(message.chat.id, "I'm sorry :S I wasn't able to find the cities!")		
+		return 5
 	if len(cityList) > 0:
 		try:
 			city = cityList[0]
 			for element in cityList:
-				if str(element) == input:
+				if str(element) == input_city:
 					city = str(element)
 			station = getPriceGasByCity(city, command)
-			bot.reply_to(message, "The cheapest gas station of " + station.getCity() + " is in the street: " + station.getAddress() + ". Price: " + station.getPrice())
+			bot.send_message(message.chat.id, "The cheapest gas station of " + station.getCity() + " is in the street: " + station.getAddress() + ". Price: " + station.getPrice())
 		except:
-			bot.reply_to(message, 'Could not find the prices')
+			bot.send_message(message.chat.id, "I couldn't find the prices :'(")
 			return 2
 	else:
-		bot.reply_to(message, 'Could not find the city')
+		bot.send_message(message.chat.id, "I couldn't find the city :'(")
 		return 1
 
 	try:
@@ -155,42 +169,54 @@ def gasCommand(message):
 		location = telebot.types.Location(map_coords[0], map_coords[1])
 		bot.send_location(message.chat.id,map_coords[0], map_coords[1])
 	except:
-		bot.reply_to(message, 'Could not generate the map')
+		bot.send_message(message.chat.id, "I couldn't generate the map :'(")
 		return 3
 
+# Function that gets called when a location is sent
 @bot.message_handler(content_types=['location'])
 def handle_location(message):
 	lng = str(message.location.longitude)
 	lat = str(message.location.latitude)
 	try:
 		station = getPriceGasByLocation(lat,lng)
-		bot.reply_to(message, "The cheapest gas station of your area is in: " + station.getAddress() + ", " + station.getCity() + ". Name: " + station.getName() + ". Price: " + station.getPrice())
+		bot.send_message(message.chat.id, "The cheapest gas station of your area is in: " + station.getAddress() + ", " + station.getCity() + ". Name: " + station.getName() + ". Price: " + station.getPrice())
 	except:
-		bot.reply_to(message, "Could not get the gas station info. Make sure you sent a location of USA or Canada")
+		bot.send_message(message.chat.id, "I couldn't get the gas station info. Just make sure you sent a location of USA or Canada")
 		return 1
 	try:
 		map_coords = getMapLocation(station.getAddress(), station.getCity())
 		location = telebot.types.Location(map_coords[0], map_coords[1])
 		bot.send_location(message.chat.id,map_coords[0], map_coords[1])
 	except:
-		bot.reply_to(message, 'Could not generate the map')
+		bot.send_message(message.chat.id, "I couldn't generate the map :'(")
 		return 2
 	
 
+# Function that gets called when it recieves a message
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_all(message):
 	input = message.text
 	try:
 		cityList = getCities(input)
-		if len(cityList) > 1:
+		if len(cityList) > 0:
 			markup = telebot.types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
 			for element in cityList:
 				item = telebot.types.KeyboardButton('/gas ' + str(element))
 				item2 = telebot.types.KeyboardButton('/diesel ' + str(element))
 				markup.add(item, item2)
 			bot.send_message(message.chat.id, text='Choose city and type:', reply_markup=markup)
+		else:
+			bot.send_message(message.chat.id, "I couldn't find the city. Does it exist? :v")		
 	except:
-		bot.reply_to(message, "Problem getting city name, make sure you didn't include special characters")
+		bot.send_message(message.chat.id, "I had a problem getting the cities, did you include any special character?")
 		return 1
 
-bot.polling()
+
+# Queries will be held every 3 seconds
+def main():
+	bot.polling()
+	while True:
+		time.sleep(3)
+
+if __name__ == '__main__':
+	main()
